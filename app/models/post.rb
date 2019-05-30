a4c01bd484ef8562ec9f13e4eaa0722616541e0c
Laura Felce <crepu83@yahoo.it>
2019-05-30 08:54:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,10 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+end    
 
+
+      class MyValidator < ActiveModel::Validator
 
       def validate(post)
         unless post.title.include? "Won't Believe || Secret || Top [number] || Guess"
