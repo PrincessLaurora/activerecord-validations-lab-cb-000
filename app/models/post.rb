366a61f57eb58c0a56065325e5653c37694cf9e3
Laura Felce <crepu83@yahoo.it>
2019-05-30 08:47:32 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,18 @@
 class Post < ActiveRecord::Base
+
+  include ActiveModel::Validations
+  validates_with MyValidator
+
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    class MyValidator < ActiveModel::Validator
+    def validate(post)
+      unless post.title.include? '("Won't Believe" || "Secret" || "Top [number]" || "Guess")'
+        post.errors[:title] << 'It's not clickbait-y!'
+      end
+    end
+  end
 end
