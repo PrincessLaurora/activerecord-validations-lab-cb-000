a541774a4de7788f8a60335d77df0b70f0a8628b
Laura Felce <crepu83@yahoo.it>
2019-05-30 08:58:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
 
+  include ActiveModel::Validations
+  validates_with MyValidator
+
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
@@ -14,8 +17,3 @@ end
       end
     end
   end
-
-class Post
-  include ActiveModel::Validations
-  validates_with MyValidator
-end
