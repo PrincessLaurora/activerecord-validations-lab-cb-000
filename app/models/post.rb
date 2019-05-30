96f4c5a27bf733ad6db974a3e4403a503e34b38d
Laura Felce <crepu83@yahoo.it>
2019-05-30 09:02:14 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ActiveRecord::Base
     include ActiveModel::Validations
     validates_with MyValidator
       def validate(post)
-        if post.title == nil && post.title.include?("Won't Believe || Secret || Top [number] || Guess")
+        if post.title == nil && post.title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
           post.errors[:title] << "It's not clickbait-y!"
       end
     end
