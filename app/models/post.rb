226ac1723101fe92b1bfc215fb0600c154dac59b
Laura Felce <crepu83@yahoo.it>
2019-05-30 08:50:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,10 +9,10 @@ class Post < ActiveRecord::Base
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 
     class MyValidator < ActiveModel::Validator
-    def validate(post)
-      unless post.title.include? "Won't Believe || Secret || Top [number] || Guess"
-        post.errors[:title] << "It's not clickbait-y!""
+      def validate(post)
+        unless post.title.include? "Won't Believe || Secret || Top [number] || Guess"
+          post.errors[:title] << "It's not clickbait-y!""
       end
     end
-  end
+
 end
